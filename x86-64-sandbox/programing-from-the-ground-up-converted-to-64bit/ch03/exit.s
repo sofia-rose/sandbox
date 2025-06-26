06f2c2b239029fea1eaa1dce52a8607e139f9ac4
sofia <sofrose@proton.me>
2025-06-26 02:30:45 -0400

    x86-64-sandbox: pgu: adds exit.s

---
+++ x86-64-sandbox/programing-from-the-ground-up-converted-to-64bit/ch03/exit.s
@@ -0,0 +1,22 @@
+; PURPOSE: Simple program that exits and returns a status code back to the
+;          Linux kernel.
+;
+
+; INPUT: none
+;
+
+; OUTPUT: Returns a status code. This can b viewed by typing `echo $?` after
+;         running the program.
+
+; VARIABLES:
+;             %rax holds the system call number
+;             %rdi holds the return status
+
+global _start
+
+section .text
+
+_start:
+  mov rax, 60
+  mov rdi, 42
+  syscall
