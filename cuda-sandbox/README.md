ece2f029496f81648a31af85e5d2b4e16d02bd09
sofia <sofrose@proton.me>
2025-06-30 00:55:47 -0400

    add cuda docker file

---
+++ cuda-sandbox/README.md
@@ -0,0 +1,15 @@
+# cuda-sandbox
+
+## Hardware/Environment
+
+* Nvidia Card: GeForce RTX 4060
+* Distro: Debian 12.11
+* Kernel: 6.1.0-37-amd64
+* Nvidia Driver Version: 535.247.01
+* CUDA Version: 12.2
+
+## References
+
+* https://hub.docker.com/r/nvidia/cuda
+* https://docs.nvidia.com/datacenter/cloud-native/container-toolkit/latest/install-guide.html
+* https://docs.nvidia.com/cuda/archive/12.2.2/
---
+++ cuda-sandbox/enter-dev.sh
@@ -0,0 +1,35 @@
+#!/usr/bin/env bash
+
+set -eu
+
+export USERNAME="$(whoami)"
+export USER_UID="$(id -u)"
+export USER_GID="$(id -g)"
+
+export HOST_WORKSPACE="${HOST_WORKSPACE:-$PWD}"
+export CONTAINER_WORKSPACE="${CONTAINER_WORKSPACE:-/home/${USERNAME}/workspace}"
+export IMAGE_NAME=cuda-sandbox
+export IMAGE_TAG=development
+
+docker build \
+  --progress plain \
+  --build-arg USERNAME \
+  --build-arg USER_UID \
+  --build-arg USER_GID \
+  --platform linux/amd64 \
+  --tag "${IMAGE_NAME}:${IMAGE_TAG}" \
+  .
+
+docker run \
+  --platform linux/amd64 \
+  --interactive \
+  --tty \
+  --rm \
+  --runtime nvidia \
+  --gpus all \
+  --user "${USERNAME}" \
+  --env TERM="xterm-256color" \
+  --volume "${HOST_WORKSPACE}:${CONTAINER_WORKSPACE}" \
+  --workdir "${CONTAINER_WORKSPACE}" \
+  "${IMAGE_NAME}:${IMAGE_TAG}" \
+  bash
