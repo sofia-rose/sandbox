5c842daa0b4ab1e2d1ba7aab206f13ecc75a57fd
sofia <sofrose@proton.me>
2025-07-21 20:47:20 -0400

    adds golang dev container

---
+++ golang/enter-dev.sh
@@ -0,0 +1,33 @@
+#!/usr/bin/env bash
+
+set -eu
+
+export USERNAME="$(whoami)"
+export USER_UID="$(id -u)"
+export USER_GID="$(id -g)"
+
+export HOST_WORKSPACE="${HOST_WORKSPACE:-$PWD}"
+export CONTAINER_WORKSPACE="${CONTAINER_WORKSPACE:-/home/${USERNAME}/workspace}"
+export IMAGE_NAME=golang-sandbox
+export IMAGE_TAG=development
+
+docker build \
+  --progress plain \
+  --build-arg USERNAME \
+  --build-arg USER_UID \
+  --build-arg USER_GID \
+  --platform linux/amd64 \
+  --tag "${IMAGE_NAME}:${IMAGE_TAG}" \
+  .
+
+docker run \
+  --platform linux/amd64 \
+  --interactive \
+  --tty \
+  --rm \
+  --user "${USERNAME}" \
+  --env TERM="xterm-256color" \
+  --volume "${HOST_WORKSPACE}:${CONTAINER_WORKSPACE}" \
+  --workdir "${CONTAINER_WORKSPACE}" \
+  "${IMAGE_NAME}:${IMAGE_TAG}" \
+  bash
